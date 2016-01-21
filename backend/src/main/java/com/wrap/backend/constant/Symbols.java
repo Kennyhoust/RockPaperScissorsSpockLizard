4e74af97550ada0c0e441422c916119765066b50
Kenny <kennyhoust@gmail.com>
2016-01-22 00:30:27 +0800

    Set Rules

--- backend/src/main/java/com/wrap/backend/constant/Symbols.java
+++ backend/src/main/java/com/wrap/backend/constant/Symbols.java
@@ -6,22 +6,35 @@ public final class Symbols {
 	public final static int SCISSORS = 2;
 	public final static int SPOCK = 3;
 	public final static int LIZARD = 4;
-	private static String DETERMINER[][] = new String[5][5];
+	
+	
+
+	public final static int crushes = 1;
+	public final static int covers = 2;
+	public final static int disaproves = 3;
+	public final static int cuts = 4;
+	public final static int decapitates = 5;
+	public final static int vaporizes = 6;
+	public final static int smashes = 7;
+	public final static int eats = 8;	
+	public final static int poisons = 9;	
+	private static int DETERMINER[][] = new int[5][5];
 	static {
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
-				DETERMINER[i][j] = "";
+				DETERMINER[i][j] = 0;
 			}
-			DETERMINER[ROCK][SCISSORS] = "crushes";
-			DETERMINER[ROCK][LIZARD] = "crushes";
-			DETERMINER[PAPER][ROCK] = "covers";
-			DETERMINER[PAPER][SPOCK] = "disaproves";
-			DETERMINER[SCISSORS][PAPER] = "cuts";
-			DETERMINER[SCISSORS][LIZARD] = "decapitates";
-			DETERMINER[SPOCK][ROCK] = "vaporizes";
-			DETERMINER[SPOCK][SCISSORS] = "smashes";
-			DETERMINER[LIZARD][PAPER] = "eats";
-			DETERMINER[LIZARD][SPOCK] = "poisons";
+			DETERMINER[ROCK][SCISSORS] = crushes;
+			DETERMINER[ROCK][LIZARD] = crushes;
+			DETERMINER[PAPER][ROCK] = covers;
+			DETERMINER[PAPER][SPOCK] = disaproves;
+			DETERMINER[SCISSORS][PAPER] = cuts;
+			DETERMINER[SCISSORS][LIZARD] = decapitates;
+			DETERMINER[SPOCK][ROCK] = vaporizes;
+			DETERMINER[SPOCK][SCISSORS] = smashes;
+			DETERMINER[LIZARD][PAPER] = eats;
+			DETERMINER[LIZARD][SPOCK] = poisons;
 		}
 	}
+	
 }
