e6ac198c73ff4f7994fb58aee0c51ad11a513272
Kenny <kennyhoust@gmail.com>
2016-01-22 00:21:38 +0800

    Set Rules

--- backend/src/main/java/com/wrap/backend/constant/Symbols.java
+++ backend/src/main/java/com/wrap/backend/constant/Symbols.java
@@ -6,4 +6,22 @@ public final class Symbols {
 	public final static int SCISSORS = 2;
 	public final static int SPOCK = 3;
 	public final static int LIZARD = 4;
+	private static String DETERMINER[][] = new String[5][5];
+	static {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				DETERMINER[i][j] = "";
+			}
+			DETERMINER[ROCK][SCISSORS] = "crushes";
+			DETERMINER[ROCK][LIZARD] = "crushes";
+			DETERMINER[PAPER][ROCK] = "covers";
+			DETERMINER[PAPER][SPOCK] = "disaproves";
+			DETERMINER[SCISSORS][PAPER] = "cuts";
+			DETERMINER[SCISSORS][LIZARD] = "decapitates";
+			DETERMINER[SPOCK][ROCK] = "vaporizes";
+			DETERMINER[SPOCK][SCISSORS] = "smashes";
+			DETERMINER[LIZARD][PAPER] = "eats";
+			DETERMINER[LIZARD][SPOCK] = "poisons";
+		}
+	}
 }
