80ac6b2a77d34203c1f166aa4405ec12a69a3beb
Kenny <kennyhoust@gmail.com>
2016-01-22 00:42:19 +0800

    Refactor

--- backend/src/main/java/com/wrap/backend/constant/Deteminer.java
+++ backend/src/main/java/com/wrap/backend/constant/Deteminer.java
@@ -2,25 +2,25 @@ package com.wrap.backend.constant;
 
 public class Deteminer {
 
-	private static int DETERMINER[][] = new int[5][5];
+	private static Action DETERMINER[][] = new Action[5][5];
 	static {
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
-				DETERMINER[i][j] = 0;
+				DETERMINER[i][j] = Action.standoff;
 			}
-			DETERMINER[Symbols.ROCK][Symbols.SCISSORS] = Symbols.crushes;
-			DETERMINER[Symbols.ROCK][Symbols.LIZARD] = Symbols.crushes;
-			DETERMINER[Symbols.PAPER][Symbols.ROCK] = Symbols.covers;
-			DETERMINER[Symbols.PAPER][Symbols.SPOCK] = Symbols.disaproves;
-			DETERMINER[Symbols.SCISSORS][Symbols.PAPER] = Symbols.cuts;
-			DETERMINER[Symbols.SCISSORS][Symbols.LIZARD] = Symbols.decapitates;
-			DETERMINER[Symbols.SPOCK][Symbols.ROCK] = Symbols.vaporizes;
-			DETERMINER[Symbols.SPOCK][Symbols.SCISSORS] = Symbols.smashes;
-			DETERMINER[Symbols.LIZARD][Symbols.PAPER] = Symbols.eats;
-			DETERMINER[Symbols.LIZARD][Symbols.SPOCK] = Symbols.poisons;
+			DETERMINER[Symbols.ROCK][Symbols.SCISSORS] = Action.crushes;
+			DETERMINER[Symbols.ROCK][Symbols.LIZARD] = Action.crushes;
+			DETERMINER[Symbols.PAPER][Symbols.ROCK] = Action.covers;
+			DETERMINER[Symbols.PAPER][Symbols.SPOCK] = Action.disaproves;
+			DETERMINER[Symbols.SCISSORS][Symbols.PAPER] = Action.cuts;
+			DETERMINER[Symbols.SCISSORS][Symbols.LIZARD] = Action.decapitates;
+			DETERMINER[Symbols.SPOCK][Symbols.ROCK] = Action.vaporizes;
+			DETERMINER[Symbols.SPOCK][Symbols.SCISSORS] = Action.smashes;
+			DETERMINER[Symbols.LIZARD][Symbols.PAPER] = Action.eats;
+			DETERMINER[Symbols.LIZARD][Symbols.SPOCK] = Action.poisons;
 		}
 	}
-	public static int play(int play,int crpty){
+	public static Action play(int play,int crpty){
 		return DETERMINER[play][crpty];
 	}
 
--- backend/src/main/java/com/wrap/backend/constant/Symbols.java
+++ backend/src/main/java/com/wrap/backend/constant/Symbols.java
@@ -9,15 +9,7 @@ public final class Symbols {
 	
 	
 
-	public final static int crushes = 1;
-	public final static int covers = 2;
-	public final static int disaproves = 3;
-	public final static int cuts = 4;
-	public final static int decapitates = 5;
-	public final static int vaporizes = 6;
-	public final static int smashes = 7;
-	public final static int eats = 8;	
-	public final static int poisons = 9;	
+
 
 	
 }
