5d86a29e922ff2e6b36eec0d7e85a935c86ba7f4
Kenny <kennyhoust@gmail.com>
2016-01-22 01:01:27 +0800

    int gui

--- frontend/src/main/java/com/wrap/frontend/App.java
+++ frontend/src/main/java/com/wrap/frontend/App.java
@@ -1,5 +1,8 @@
 package com.wrap.frontend;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,30 @@ public class App
 {
     public static void main( String[] args )
     {
-	        System.out.println( "Hello World!" );
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+
+
+        });
+    }
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("“Rock, Paper, Scissors, Spock, Lizard");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Rock, Paper, Scissors, Spock, Lizard");
+        frame.getContentPane().add(label);
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
     }
 }
