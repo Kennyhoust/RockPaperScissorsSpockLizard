88833de18841f10986c5e3715cf14889f4f8e07a
Kenny <kennyhoust@gmail.com>
2016-01-22 00:01:43 +0800

    initialize symbols constants

---
+++ backend/src/main/java/com/wrap/backend/constant/Symbols.java
@@ -0,0 +1,9 @@
+package com.wrap.backend.constant;
+
+public final class Symbols {
+	public final static int ROCK = 0;
+	public final static int PAPER = 1;
+	public final static int SCISSORS = 2;
+	public final static int SPOCK = 3;
+	public final static int LIZARD = 4;
+}
