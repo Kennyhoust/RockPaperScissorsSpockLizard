47b7a765fe9feaf144151e020bd78da758690edb
Kenny <kennyhoust@gmail.com>
2016-01-21 23:49:00 +0800

    initialize project

---
+++ frontend/src/main/java/com/wrap/frontend/App.java
@@ -0,0 +1,13 @@
+package com.wrap.frontend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
