cf991ca66fbca839498d7a7bf8cff6e0ca54c50c
Kenny <kennyhoust@gmail.com>
2016-01-22 22:19:00 +0800

    Score board singleton

--- frontend/src/main/java/com/wrap/frontend/ScoreBoard.java
+++ frontend/src/main/java/com/wrap/frontend/ScoreBoard.java
@@ -22,7 +22,7 @@ public class ScoreBoard extends JPanel {
 	public ScoreBoard() {
 		super(new GridLayout(1, 0));
 
-		JTable table = new JTable(new MyTableModel());
+		JTable table = new JTable(ScoreBoardModel.getInstance());
 		table.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
 			@Override
 			public Component getTableCellRendererComponent(JTable table,
@@ -44,82 +44,5 @@ public class ScoreBoard extends JPanel {
 		add(scrollPane);
 	}
 
-	class MyTableModel extends AbstractTableModel {
-		private String[] columnNames = { "You", "Comp" };
-		private Object[][] data = {
-				{ "Kathy", "Smith" }
-				};
 
-		public int getColumnCount() {
-			return columnNames.length;
-		}
-
-		public int getRowCount() {
-			return data.length;
-		}
-
-		public String getColumnName(int col) {
-			return columnNames[col];
-		}
-
-		public Object getValueAt(int row, int col) {
-			return data[row][col];
-		}
-
-		/*
-		 * JTable uses this method to determine the default renderer/ editor for
-		 * each cell. If we didn't implement this method, then the last column
-		 * would contain text ("true"/"false"), rather than a check box.
-		 */
-		public Class getColumnClass(int c) {
-			return getValueAt(0, c).getClass();
-		}
-
-		/*
-		 * Don't need to implement this method unless your table's editable.
-		 */
-		public boolean isCellEditable(int row, int col) {
-			// Note that the data/cell address is constant,
-			// no matter where the cell appears onscreen.
-			if (col < 2) {
-				return false;
-			} else {
-				return true;
-			}
-		}
-
-		/*
-		 * Don't need to implement this method unless your table's data can
-		 * change.
-		 */
-		public void setValueAt(Object value, int row, int col) {
-			if (DEBUG) {
-				System.out.println("Setting value at " + row + "," + col
-						+ " to " + value + " (an instance of "
-						+ value.getClass() + ")");
-			}
-
-			data[row][col] = value;
-			fireTableCellUpdated(row, col);
-
-			if (DEBUG) {
-				System.out.println("New value of data:");
-				printDebugData();
-			}
-		}
-
-		private void printDebugData() {
-			int numRows = getRowCount();
-			int numCols = getColumnCount();
-
-			for (int i = 0; i < numRows; i++) {
-				System.out.print("    row " + i + ":");
-				for (int j = 0; j < numCols; j++) {
-					System.out.print("  " + data[i][j]);
-				}
-				System.out.println();
-			}
-			System.out.println("--------------------------");
-		}
-	}
 }
---
+++ frontend/src/main/java/com/wrap/frontend/ScoreBoardModel.java
@@ -0,0 +1,83 @@
+package com.wrap.frontend;
+
+import javax.swing.table.AbstractTableModel;
+
+public class ScoreBoardModel extends AbstractTableModel {
+	private String[] columnNames = { "You", "Comp" };
+	private Object[][] data = {
+			{ "Kathy", "Smith" }
+			};
+	public static ScoreBoardModel scoreBoardModel;
+	public static ScoreBoardModel getInstance(){
+		if(scoreBoardModel == null){
+			scoreBoardModel = new ScoreBoardModel();
+		}
+		return scoreBoardModel;
+	}
+	private ScoreBoardModel(){
+		
+	}
+	public int getColumnCount() {
+		return columnNames.length;
+	}
+
+	public int getRowCount() {
+		return data.length;
+	}
+
+	public String getColumnName(int col) {
+		return columnNames[col];
+	}
+
+	public Object getValueAt(int row, int col) {
+		return data[row][col];
+	}
+
+	/*
+	 * JTable uses this method to determine the default renderer/ editor for
+	 * each cell. If we didn't implement this method, then the last column
+	 * would contain text ("true"/"false"), rather than a check box.
+	 */
+	public Class getColumnClass(int c) {
+		return getValueAt(0, c).getClass();
+	}
+
+	/*
+	 * Don't need to implement this method unless your table's editable.
+	 */
+	public boolean isCellEditable(int row, int col) {
+		// Note that the data/cell address is constant,
+		// no matter where the cell appears onscreen.
+		if (col < 2) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	/*
+	 * Don't need to implement this method unless your table's data can
+	 * change.
+	 */
+	public void setValueAt(Object value, int row, int col) {
+
+		data[row][col] = value;
+		fireTableCellUpdated(row, col);
+
+
+	}
+
+	private void printDebugData() {
+		int numRows = getRowCount();
+		int numCols = getColumnCount();
+
+		for (int i = 0; i < numRows; i++) {
+			System.out.print("    row " + i + ":");
+			for (int j = 0; j < numCols; j++) {
+				System.out.print("  " + data[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("--------------------------");
+	}
+}
