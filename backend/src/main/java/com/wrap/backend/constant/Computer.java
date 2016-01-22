890f7e3d67923d1fde76fae5e0b5a8de2e74b219
Kenny <kennyhoust@gmail.com>
2016-01-22 22:45:33 +0800

    show result

--- backend/src/main/java/com/wrap/backend/constant/Computer.java
+++ backend/src/main/java/com/wrap/backend/constant/Computer.java
@@ -1,7 +1,13 @@
 package com.wrap.backend.constant;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class Computer {
-	public static Symbols getSymbols(){
-		return Symbols.LIZARD;
+	public static Symbols getSymbols(List<Symbols> list){
+		List<Symbols> symbolsList =Arrays.asList(Symbols.values());
+		Random ran = new Random();
+		return symbolsList.get(ran.nextInt(5));
 	}
 }
