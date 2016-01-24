d5174e5c117bcb1b811023a34aabe6b1d5d558ef
Kenny <kennyhoust@gmail.com>
2016-01-24 23:55:07 +0800

    Add Image

--- backend/src/main/java/com/wrap/backend/Computer.java
+++ backend/src/main/java/com/wrap/backend/Computer.java
@@ -9,7 +9,7 @@ import java.util.Random;
 public class Computer {
 	private Map<Symbols,Integer> hisgram = new HashMap<Symbols,Integer>();
 	private static Computer comp;
-	
+	private Symbols highestsymbol;
 	private Computer(){
 		
 	}
@@ -20,17 +20,25 @@ public class Computer {
 	}
 	public Symbols getSymbols(List<Symbols> list){
 		recordHisgram(list);
-		Symbols resultSym =getDefaultSym();
+		Symbols resultSym =null;
 		Integer palyerMostTimeChoice = 0;
 		for(Symbols sym:hisgram.keySet()){
-			System.out.println(sym.name()+":"+hisgram.get(sym));
+			
 			if(hisgram.get(sym)>palyerMostTimeChoice){
 				resultSym = sym;
 				palyerMostTimeChoice = hisgram.get(sym);
 			}
 		}
+		if(resultSym!=null)
+			highestsymbol = resultSym;
+		else
+			resultSym=this.getDefaultSym();
 		Symbols counterparty = findCounterparty(resultSym);
-		System.out.println("==============");
+		System.out.println("==========");
+		for(Map.Entry<Symbols, Integer> sym:hisgram.entrySet()){
+			System.out.println(sym.getKey()+":"+sym.getValue());
+		}
+		
 		return 	counterparty;
 	}
 	public Symbols getDefaultSym() {
@@ -44,10 +52,15 @@ public class Computer {
 		Symbols symbols = list.get(0);
 
 		if(hisgram.containsKey(symbols)){
-				hisgram.put(symbols,hisgram.get(symbols)+1);
+			if(!isHightest(symbols))
+			hisgram.put(symbols,hisgram.get(symbols)+1);
 		}else{
-			hisgram.put(symbols, 0);
+			hisgram.put(symbols, 1);
 		}
+	}
+	private boolean isHightest(Symbols symbols) {
+		// TODO Auto-generated method stub
+		return symbols.equals(highestsymbol);
 	}
 	public Symbols findCounterparty(Symbols resultSym){
 		int guess = 0;
